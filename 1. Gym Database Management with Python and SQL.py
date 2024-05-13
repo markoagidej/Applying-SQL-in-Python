6ab5d0fcc8ec1d97be1c38d88725b04c815f73e7
Marko <markogidej@yahoo.com>
2024-05-13 12:02:03 -0400

    Question 1 tested and complete

--- 1. Gym Database Management with Python and SQL.py
+++ 1. Gym Database Management with Python and SQL.py
@@ -6,42 +6,71 @@ from mysql.connector import Error
 def connect_db():
     # Connect to DB and return the connection
     try:
-        conn = mysql.connector.connect("applying_sql_in_python", "root", "*963.MADEupPASSWORD", "localhost")
+        conn = mysql.connector.connect(
+            database="applying_sql_in_python",
+            user="root",
+            password="PASSWORD",
+            host="localhost"
+            )
         if conn.is_connected():
             return conn
     except Error as e:
+        print("Problem connection to server.")
         print(f"Error: {e}")
         return None
+    
+def close_connection(connection, cursor):
+    connection.close()
+    cursor.close()
 
 # Task 1: Add a Member
-def add_member(name, age, trainer_id): # no "id" included because the database will handle auto incrementing a unique id for Members
+def add_member(name, age, trainer_id): # no "id" included because the database will handle auto incrementing a unique id for members
     conn = connect_db()
     if conn is not None:
         try:
             cursor = conn.cursor()
             member_info = (name, age, trainer_id)
-            query = "INSERT INTO Members (name, age, trainer_id) VALUES (%s, %s, %s)"
+            query = "INSERT INTO members (name, age, trainer_id) VALUES (%s, %s, %s)"
             cursor.execute(query, member_info)
             conn.commit()
             print(f"New member {name} added!")
+        except Error as e:
+            print("Problem adding member.")
+            print(f"Error: {e}")
         finally:
-            cursor.close()
-            conn.close()
+            close_connection(conn, cursor)
 
 # Task 2: Add a Workout Session
 def add_workout_session(member_id, date, duration, calories_burned):
     conn = connect_db()
     if conn is not None:
+        cursor = conn.cursor()
+        # First try to see if there is a member with the requested member_id
         try:
-            cursor = conn.cursor()
+            memeber_id_value = (member_id,)
+            search_query = "SELECT * FROM members WHERE id = (%s)"
+            cursor.execute(search_query, memeber_id_value)
+            if not cursor.fetchone():
+                print(f"There was no member found with id of \'{member_id}\'!")
+                close_connection(conn, cursor)
+                return
+        except Error as e:
+            print("Problem searching for member.")
+            print(f"Error: {e}")
+            close_connection(conn, cursor)
+            return
+        # Try to insert the new workout session
+        try:
             workout_info = (member_id, date, duration, calories_burned)
-            query = "INSERT INTO WorkoutSessions (member_id, date, duration_minutes, calories_burned) VALUES (%s, %s, %s, %s)"
+            query = "INSERT INTO workout_sessions (member_id, date, duration_minutes, calories_burned) VALUES (%s, %s, %s, %s)"
             cursor.execute(query, workout_info)
             conn.commit()
             print(f"New workout added for member: {member_id} added!")
+        except Error as e:
+            print("Problem adding a workout session.")
+            print(f"Error: {e}")
         finally:
-            cursor.close()
-            conn.close()
+            close_connection(conn, cursor)
 
 # Task 3: Updating Member Information
 def update_member_age(member_id, age):
@@ -50,27 +79,32 @@ def update_member_age(member_id, age):
         cursor = conn.cursor()
         # First try to see if there is a member with the requested member_id
         try:
-            age_value = (age)
-            search_query = "SELECT * FROM Members WHERE age = (%s)"
-            cursor.execute(search_query, age_value)
-            if cursor.fetchone():
+            memeber_id_value = (member_id,)
+            search_query = "SELECT * FROM members WHERE id = (%s)"
+            cursor.execute(search_query, memeber_id_value)
+            if not cursor.fetchone():
                 print(f"There was no member found with id of \'{member_id}\'!")
+                close_connection(conn, cursor)
                 return
-        except:
-            print("Problem searching for member")
-            cursor.close()
-            conn.close()
+        except Error as e:
+            print("Problem searching for member.")
+            print(f"Error: {e}")
+            close_connection(conn, cursor)
             return
         # Try to update the age of requested member
         try:
-            update_info = (member_id, age)
-            query = "UPDATE Members VALUES (%s, %s, %s, %s)"
+            update_info = (age, member_id)
+            query = "UPDATE members SET age = %s WHERE id = %s"
             cursor.execute(query, update_info)
             conn.commit()
-            print(f"New workout added for member: {member_id} added!")
+            print(f"Age of member number \'{member_id}\' updated to {age}!")
+        except Error as e:
+            print("Problem updating member's age.")
+            print(f"Error: {e}")
+            close_connection(conn, cursor)
+            return
         finally:
-            cursor.close()
-            conn.close()
+            close_connection(conn, cursor)
 
 # Task 4: Delete a Workout Session
 def delete_workout_session(session_id):
@@ -79,26 +113,26 @@ def delete_workout_session(session_id):
         cursor = conn.cursor()
         # First try to see if there is a session with the requested session_id
         try:
-            session_value = (session_id)
-            search_query = "SELECT * FROM WorkoutSessions WHERE session_id = (%s)"
+            session_value = (session_id,)
+            search_query = "SELECT * FROM workout_sessions WHERE id = (%s)"
             cursor.execute(search_query, session_value)
-            if cursor.fetchone():
+            if not cursor.fetchone():
                 print(f"There was workout session found with id of \'{session_id}\'!")
+                close_connection(conn, cursor)
                 return
-        except:
-            print("Problem searching for session")
-            cursor.close()
-            conn.close()
+        except Error as e:
+            print("Problem searching for session.")
+            print(f"Error: {e}")
+            close_connection(conn, cursor)
             return
         # Try to delete teh session by session_id
         try:
-            query = "DELETE FROM WorkoutSessions WHERE session_id = (%s)"
+            query = "DELETE FROM workout_sessions WHERE id = (%s)"
             cursor.execute(query, session_value)
             conn.commit()
-            print(f"Workout Session \'{session_id}\' delted!")
+            print(f"Workout Session \'{session_id}\' deleted!")
         finally:
-            cursor.close()
-            conn.close()
+            close_connection(conn, cursor)
 
 # Main
 def main():
---
+++ 2. Advanced Data Analysis in Gym Management System.py
@@ -0,0 +1,10 @@
+ # 2. Advanced Data Analysis in Gym Management System
+
+# Task 1: SQL DISTINCT Usage
+
+
+# Task 2: SQL COUNT Functionality
+
+
+# Task 3: SQL BETWEEN Usage
+
